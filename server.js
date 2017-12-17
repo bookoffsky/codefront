17faa270ad725160656c1c7e1ca7f8e6d8cba212
bookoffsky <Bookoffsky@gmail.com>
2017-12-17 21:39:43 +0300

    sd

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ const server = http.createServer(function (request,response) {
     response.end(html);
 }
 });
-server.listen(3000);
+console.log('port =', process.env.PORT);
+server.listen(process.env.PORT || 3000);
 console.log('Server start!');
